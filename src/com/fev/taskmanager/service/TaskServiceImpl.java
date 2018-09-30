6235f2f011e8f096ca5268fb280a74aafbfd5491
Feven Mamo <fevuhabesha@gmail.com>
2018-09-29 21:32:23 -0500

    add task service

---
+++ src/com/fev/taskmanager/service/TaskServiceImpl.java
@@ -0,0 +1,18 @@
+package com.fev.taskmanager.service;
+
+import java.util.List;
+
+import com.fev.taskmanager.model.Task;
+import com.fev.taskmanager.repository.TaskRepository;
+import com.fev.taskmanager.repository.TaskRepositoryImpl;
+
+public class TaskServiceImpl implements TaskService {
+	TaskRepository taskRepository = new TaskRepositoryImpl();
+
+	@Override
+	public List<Task> getTasks() {
+		List<Task> tasks = taskRepository.fetchAllTasks();
+		return tasks;
+	}
+
+}
